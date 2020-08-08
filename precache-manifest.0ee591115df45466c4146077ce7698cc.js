78898f6e6d79235a8dfa69666031355a9a38b918
Park JongIk <pji3504@naver.com>
2020-08-08 23:13:45 +0900

    Updates

---
+++ precache-manifest.0ee591115df45466c4146077ce7698cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d4337490475d9bb9dc247faf4165f0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "cf4afd429ae55ba771ee",
+    "url": "/movie_app_2020/static/css/main.b896c3ef.chunk.css"
+  },
+  {
+    "revision": "bee32acad7d71db01dca",
+    "url": "/movie_app_2020/static/js/2.30b7f8fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.30b7f8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf4afd429ae55ba771ee",
+    "url": "/movie_app_2020/static/js/main.b193579e.chunk.js"
+  },
+  {
+    "revision": "79e82b1f7129a21ddd71",
+    "url": "/movie_app_2020/static/js/runtime-main.ad33c744.js"
+  }
+]);
